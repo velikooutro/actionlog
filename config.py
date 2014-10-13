c105ef69f4596961faeea37a0268dc229aed238e
Veliko Outro <veliko.outro@gmail.com>
2014-10-12 21:06:49 -0400

    adding files to repo

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+
+import os
+
+# grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'actionlog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+CSRF_ENABLED = True
+SECRET_KEY = 'veliko.outro'
+
+# defines the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
